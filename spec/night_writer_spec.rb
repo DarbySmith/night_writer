ce2db19370a67e3ca8f154953c58f8a3d513403b
Darby Smith <newman.darby2@gmail.com>
2022-09-23 11:43:57 -0400

    Refactor all tests with text files

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 RSpec.describe NightWriter do
+  before (:each) do
+    information = {
+      message: 'message.txt',
+      braille: 'braille.txt'
+    }
+    @night_writer = NightWriter.new(information)
+  end
   
+  describe '#initialize' do
+    it 'exists' do
+      expect(@night_writer).to be_instance_of(NightWriter)
+    end
+    
+    it 'has a message' do
+      expect(@night_writer.message).to eq('message.txt')
+    end
+    
+    it 'has braille' do
+      expect(@night_writer.braille).to eq('braille.txt')
+    end
+  end
+  
+  describe '#creation_message' do
+    it 'returns a message with details for the file' do
+      expect(@night_writer.creation_message).to eq("Created 'braille.txt' containing 11 characters")
+    end
+  end
 end

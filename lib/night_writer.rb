49dbf5c14941211e53c6bacc9faf0f1c2f699de7
Darby Smith <newman.darby2@gmail.com>
2022-09-23 22:42:19 -0400

    Add convert to braille adn print braille methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+require './lib/braille'
+
 class NightWriter
   attr_reader :message_file_path, :braille_file_path
 
@@ -10,12 +12,31 @@ class NightWriter
     character_count = File.read(@message_file_path).chars.size
     "Created '#{@braille_file_path}' containing #{character_count} characters"
   end
-
+  
   def write_file_contents
     message_content = File.read(@message_file_path)
     File.open(@braille_file_path, "w") { |file| file.write(message_content) }
     File.read(@braille_file_path)
   end
+  
+  def convert_to_braille
+    @braille_alphabet = Braille.new
+    unformatted_braille = write_file_contents.chars.map do |letter|
+      if letter == " "
+        braille_letter = @braille_alphabet.alphabet[:space]
+      else
+        braille_letter = @braille_alphabet.alphabet[letter.to_sym]
+      end
+    end
+    @braille_alphabet.braille_formatter(unformatted_braille)
+  end
+
+  def print_braille_to_file(braille)
+    braille = convert_to_braille
+    File.open(@braille_file_path, "w") { |file| file.write(braille)}
+    File.read(@braille_file_path)
+  end
+  # File.open(@braille_file_path, "w") { |file| file.write(braille_text)}
 end
 
 
@@ -25,3 +46,6 @@ information = {
 }
 night_writer = NightWriter.new(information)
 p night_writer.creation_message
+night_writer.write_file_contents
+braille = night_writer.convert_to_braille
+night_writer.print_braille_to_file(braille)
